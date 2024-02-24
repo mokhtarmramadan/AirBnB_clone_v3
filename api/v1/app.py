96a34c5a7cdd53268d983c7602a138306c49f834
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-24 16:47:56 +0200

    Added 5.not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 the main module of the app
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown(exc):
     """ hanle teardown_qppcontext """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ handler for 404 errors """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
