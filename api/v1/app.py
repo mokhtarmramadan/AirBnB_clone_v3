bca7595bd829625f47e02e3abf706251408bcc2b
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-24 16:05:46 +0200

    Added the teardown func

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,18 +3,20 @@
 the main module of the app
 """
 from flask import Flask
-#from models import storage
+from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-"""
+
+
 @app.teardown_appcontext
-def teardown():
-    """ #hanle teardown_qppcontext """
-    #storage.close()
+def teardown(exc):
+    """ hanle teardown_qppcontext """
+    storage.close()
+
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,15 +5,13 @@ status route
 from api.v1.views import app_views
 from flask import jsonify, Blueprint
 
-status_bp = Blueprint('status', __name__)
 
 state = {
   "status": "OK"
 }
 
-@status_bp.route('/status', strict_slashes=False)
+
+@app_views.route('/status', strict_slashes=False)
 def status():
     """ return status """
     return jsonify(state), 200, {'Content-Type': 'application/json'}
-
-app_views.register_blueprint(status_bp)
