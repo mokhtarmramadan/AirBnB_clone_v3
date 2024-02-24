33aad4ac2d2d7e0a9b6dadbc82e263c1fdc13bd5
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-24 15:51:35 +0200

    Added a new version of the blueprint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,17 @@
 status route
 """
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, Blueprint
 
+status_bp = Blueprint('status', __name__)
 
-state = "OK"
-@app_views.route('/status', strict_slashes=False)
+state = {
+  "status": "OK"
+}
+
+@status_bp.route('/status', strict_slashes=False)
 def status():
     """ return status """
-    return jsonify({'status': state})
+    return jsonify(state), 200, {'Content-Type': 'application/json'}
+
+app_views.register_blueprint(status_bp)
