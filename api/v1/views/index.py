df2b0e0b326dc20a1b4b5e66303d1dcf5fc3dae2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-24 16:30:34 +0200

    Testing the some stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,25 @@ status route
 """
 from api.v1.views import app_views
 from flask import jsonify, Blueprint
-
-
-state = {
-  "status": "OK"
-}
-
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ return status """
+    state = {
+        "status": "OK"
+    }
     return jsonify(state)
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    "Endpoint that retrieves the number of each objects by type"
+    classes = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+        }
+    return jsonify(classes)
