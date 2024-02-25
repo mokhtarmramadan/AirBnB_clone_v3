40df8a8a271c716941def2600adac386928a5f2d
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-25 08:23:53 +0200

    Added POST, DELETE method to amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -9,9 +9,40 @@ from models.amenity import Amenity
 
 @app_views.route('/amenities', strict_slashes=False, methods=['GET'])
 def get_amenities():
+    """ Retrieves the list of all Amenity objects """
     amenities = []
     for obj in storage.all('Amenity').values():
         amenities.append(obj.to_dict())
     if amenities == []:
         abort(404)
     return amenities
+
+
+@app_views.route('/amenities/<am_id>', methods=['GET', 'DELETE'])
+def get_amenity(am_id):
+    """ Retrieves or Deletes a Amenity object """
+    for obj in storage.all('Amenity').values():
+        if obj.id == am_id:
+            if request.method == 'GET':
+                return jsonify(obj.to_dict())
+            if request.method == 'DELETE':
+                storage.delete(obj)
+                storage.save()
+                return jsonify({}), 200
+    abort(404)
+
+
+@app_views.route('/amenities', strict_slashes=False, methods=['POST'])
+def create_amenity():
+    """ Create amenity """
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+
+    amenities = []
+    new_amenity = Amenity(name=request.json['name'])
+    storage.new(new_amenity)
+    storage.save()
+    amenities.append(new_amenity.to_dict())
+    return jsonify(amenities[0]), 201
